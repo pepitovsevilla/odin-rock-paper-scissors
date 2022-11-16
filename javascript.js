27c92d1748d07574e2725ad7ddafdbe21bab88ad
Pepito Sevilla <pepitovsevilla13@gmail.com>
2022-11-16 08:47:11 +0800

    Defined function playRound.

--- javascript.js
+++ javascript.js
@@ -4,4 +4,30 @@ function getComputerChoice() {
     return computerOptions[random];
 }
 
-console.log(getComputerChoice())
+computerSelection = getComputerChoice()
+
+function playRound (computerSelection, playerSelection) {
+    computerSelection = computerSelection.toLowerCase()
+    playerSelection = playerSelection.toLowerCase()
+    console.log(computerSelection)
+    console.log(playerSelection)
+    switch(true) {
+        case computerSelection == playerSelection:
+            return "It's a tie.";
+            break;
+    
+        case (computerSelection=="rock") && (playerSelection=="scissors"):
+        case (computerSelection=="scissors") && (playerSelection=="paper"):
+        case (computerSelection=="paper") && (playerSelection=="rock"):
+            return `You Lose! ${computerSelection} beats ${playerSelection}!`;
+            break;
+
+        case (playerSelection=="rock") && (computerSelection=="scissors"):
+        case (playerSelection=="scissors") && (computerSelection=="paper"):
+        case (playerSelection=="paper") && (computerSelection=="rock"):
+            return `You Win! ${playerSelection} beats ${computerSelection}!`;
+            break;
+    }
+}
+
+console.log(playRound(getComputerChoice(),"ROCK"))
