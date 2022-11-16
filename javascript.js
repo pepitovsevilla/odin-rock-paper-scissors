f6d7884e0d7679f3d79f078e2a64dc0d14b0200f
Pepito Sevilla <pepitovsevilla13@gmail.com>
2022-11-16 10:45:01 +0800

    Define function game.

--- javascript.js
+++ javascript.js
@@ -4,13 +4,15 @@ function getComputerChoice() {
     return computerOptions[random];
 }
 
-computerSelection = getComputerChoice()
-
 function playRound (computerSelection, playerSelection) {
+ 
     computerSelection = computerSelection.toLowerCase()
     playerSelection = playerSelection.toLowerCase()
-    console.log(computerSelection)
-    console.log(playerSelection)
+    // display what the computer chose and what the player chose
+    console.log(`Computer chose: ${computerSelection}`)
+    console.log(`Player chose: ${playerSelection}`)
+
+    // check who wins the round
     switch(true) {
         case computerSelection == playerSelection:
             return "It's a tie.";
@@ -19,15 +21,66 @@ function playRound (computerSelection, playerSelection) {
         case (computerSelection=="rock") && (playerSelection=="scissors"):
         case (computerSelection=="scissors") && (playerSelection=="paper"):
         case (computerSelection=="paper") && (playerSelection=="rock"):
-            return `You Lose! ${computerSelection} beats ${playerSelection}!`;
+            return `You Lose! ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}!`;
             break;
 
         case (playerSelection=="rock") && (computerSelection=="scissors"):
         case (playerSelection=="scissors") && (computerSelection=="paper"):
         case (playerSelection=="paper") && (computerSelection=="rock"):
-            return `You Win! ${playerSelection} beats ${computerSelection}!`;
+            return `You Win! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}!`;
             break;
     }
 }
 
-console.log(playRound(getComputerChoice(),"ROCK"))
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    // play 5 rounds
+    for (let i = 0; i < 5; i++) {
+
+        let playerChoice = prompt("Rock, Paper, Scissors?")
+        let computerChoice = getComputerChoice();
+        
+        let result = (playRound(playerChoice,computerChoice)); 
+        console.log(result)
+
+        switch(true) {
+
+            case result == "It's a tie.":
+                break
+            
+            case result.slice(0,7) == `You Win`:
+                // tally score after each round
+                playerScore = playerScore + 1;
+                break
+            
+            case result.slice(0,8) == `You Lose`:
+                // tally score after each round
+                computerScore = computerScore + 1;
+                break
+
+        }
+
+        // show the running score
+        console.log(`Player: ${playerScore} | Computer: ${computerScore}`)
+
+
+    }
+
+    // announce winner after 5 rounds
+    switch(true) {
+        case playerScore > computerScore:
+            console.log("You win the game!");
+            break;
+        case playerScore > computerScore:
+            console.log("The computer wins the game!");
+            break;
+        case playerScore == computerScore:
+            console.log("The game ends in a draw!");
+            break;
+    }
+}
+
+game()
